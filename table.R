703a6b3983ae4ecc242cda8554ddceecdde38c95
minakath <minakath24@gmail.com>
2024-05-15 20:12:29 -0700

    finalized code

--- table.R
+++ table.R
@@ -1,6 +1,7 @@
 ## “Hiking Trails” + “Woods” in “Feature_Desc” column = wilderness
 
 library("tidyverse")
+library("dplyr")
 
 hiking_data <- Seattle_Parks_and_Recreation_Parks_Features_20240515 %>%
   filter(grepl("58", Feature_ID)) 
@@ -11,3 +12,10 @@ woods_data <- Seattle_Parks_and_Recreation_Parks_Features_20240515 %>%
 wilderness_data <- union(hiking_data, woods_data)
 
 
+## There are 26 Parks with both woods and hiking trails
+
+
+final_data <- wilderness_data %>%
+  group_by(Name, `Location 1`) %>%
+  summarize(Feature_Desc = paste(Feature_Desc, collapse = ", "), .groups = 'drop')
+print(final_data)
