20934f2f2b0f412eeade467ea2f85a69c19fbb2a
Mina Trice <minakath24@gmail.com>
2024-05-15 18:09:27 -0700

    started working on code to sort data

--- table.R
+++ table.R
@@ -1,1 +1,13 @@
 ## “Hiking Trails” + “Woods” in “Feature_Desc” column = wilderness
+
+library("tidyverse")
+
+hiking_data <- Seattle_Parks_and_Recreation_Parks_Features_20240515 %>%
+  filter(grepl("58", Feature_ID)) 
+
+woods_data <- Seattle_Parks_and_Recreation_Parks_Features_20240515 %>%
+  filter(grepl("37", Feature_ID))
+
+wilderness_data <- union(hiking_data, woods_data)
+
+
