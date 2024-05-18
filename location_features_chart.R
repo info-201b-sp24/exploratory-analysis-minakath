a417351a8b4c43000c14a2a2fb22d682e46b3f2c
KavyaRao42 <rao.kavya.bama@gmail.com>
2024-05-17 22:40:34 -0700

    Changed comments and variable names for chart

--- location_features_chart.R
+++ location_features_chart.R
@@ -3,9 +3,10 @@ parks_data = read.csv("Seattle_Parks_and_Recreation_Parks_Features_20240515.csv"
 # Function to calculate distance of park from Seattle city center
 calculate_distance <- function(lat1, lon1, lat2, lon2) {
   earth_radius <- 6371
-  dLat <- deg2rad(lat2 - lat1)
-  dLon <- deg2rad(lon2 - lon1)
-  a <- sin(dLat/2) * sin(dLat/2) + cos(deg2rad(lat1)) * cos(deg2rad(lat2)) * sin(dLon/2) * sin(dLon/2)
+  deg_lat <- deg2rad(lat2 - lat1)
+  deg_lon <- deg2rad(lon2 - lon1)
+  # Haversine formula
+  a <- sin(deg_lat/2) * sin(deg_lat/2) + cos(deg2rad(lat1)) * cos(deg2rad(lat2)) * sin(deg_lon/2) * sin(deg_lon/2)
   c <- 2 * atan2(sqrt(a), sqrt(1-a))
   distance_km <- earth_radius * c
   return(distance_km)
@@ -20,17 +21,17 @@ deg2rad <- function(deg) {
 city_center_lat <- 47.608013
 city_center_lon <- -122.335167
 
-# Calculate distance from Seattle city center for each park
+# Calculate distance from Seattle city center for each park and make new column
 parks_data$distance_from_center <- calculate_distance(city_center_lat, city_center_lon, parks$yPos, parks$xPos)
 
-# Make new column of how many features parks have by counting amount of times they appear in name column
+# Count how many features each park has and make new column
 parks_data$feature_count <- parks$Name %>% str_count()
 
-# Make scatterplot matrix
+# Make scatterplot
 distance_scatterplot <- ggplot(parks, aes(x = feature_count, y = distance_from_center)) +
   geom_point() +
   labs(x = "Number of Features", y = "Distance from City Center (km)") +
   ggtitle("Scatterplot of Park Features and Distance from City Center")
 
-# Print scatterplot matrix
+# Print scatterplot
 print(distance_scatterplot)
