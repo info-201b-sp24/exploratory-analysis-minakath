f5cf853b54a12fea64c6e3794ea5c355c93e8107
Mina Trice <minakath24@gmail.com>
2024-05-18 17:22:05 -0700

    updated code for chart

--- feature_count.R
+++ feature_count.R
@@ -1,7 +1,19 @@
-x_values <- seq(1, 3)
-y_values <- seq(1,3)
-
+library(dplyr)
+library(tidyr)
 library(ggplot2)
-ggplot() +
-  geom_point(aes(x=x_values, y = y_values))
+Seattle_Park_data <- read.csv("Seattle_Parks_and_Recreation_Parks_Features_20240515.csv")
+feature_counts <- Seattle_Park_data %>%
+  separate_rows (Feature_Desc, sep =",, ") %>%
+  group_by(Feature_Desc) %>%
+  summarise(Count = n()) %>%
+  filter(Count >= 25 | Count <= 3) %>%
+  arrange(desc(Count))
+feature_counts
+
+feature_chart <- ggplot(feature_counts, aes(x = reorder(Feature_Desc, -Count), y = Count)) +
+  geom_col(fill ="steelblue") +
+  labs(x = "Feature", y ="Count", title = "Ranking of Features and Amenities by Count") +
+  theme(axis.text.x = element_text(angle = 45, hjust = 1,size = 6))
+
+print(feature_chart)
 
